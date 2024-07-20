3e7f990914df057556cda3ab1379aaa66c809130
IX <borntokill.ix@gmail.com>
2024-07-20 22:55:27 +0700

    feature(remove): add callback for removing cache; remove the old method for checking expire ttl cache

--- coroutincache/coroutincache.py
+++ coroutincache/coroutincache.py
@@ -1,27 +1,27 @@
+import asyncio
 import logging
+from functools import wraps
 from time import time
 
 logger = logging.getLogger('asyncio')
+
+
+def del_cache(key):
+    logger.debug(f'Remove cache: {key=}')
 
 
 class AsyncCache:
     _cache = {}
     _ttl = {}
 
-    def __init__(self, ttl):
+    def __init__(self, ttl: float, *, refresh: bool = False):
         self.ttl = ttl
+        self.refresh = refresh
 
-    def __expire_ttl(self, created_time) -> bool:
-        return time() - created_time > self.ttl
-
-    def __refresh_cache(self):
-        keys = tuple(type(self)._ttl.keys())
-        for key in keys:
-            created_time = type(self)._ttl[key]
-            if self.__expire_ttl(created_time):
-                type(self)._ttl.pop(key)
-                type(self)._cache.pop(key)
-                logger.debug(f'Remove cache: {key=}')
+    def __del_cache(self, key):
+        type(self)._ttl.pop(key)
+        type(self)._cache.pop(key)
+        logger.debug(f'Remove cache: {key=}')
 
     async def _create_cache(self, func, *args, **kwargs):
         key = f'{func.__name__}{args}{kwargs}'
@@ -29,19 +29,20 @@ class AsyncCache:
         type(self)._ttl[key] = time()
 
     def __call__(self, func):
+        @wraps(func)
         async def wrapper(*args, **kwargs):
             key = f'{func.__name__}{args}{kwargs}'
+            loop = asyncio.get_event_loop()
+
             cached_data = type(self)._cache.get(key)
+            time_later = loop.call_later(self.ttl, self.__del_cache, key)
             if not cached_data:
                 logger.info('Create cache...')
                 await self._create_cache(func, *args, **kwargs)
-            else:
-                self.__refresh_cache()
                 cached_data = type(self)._cache.get(key)
-                if not cached_data:
-                    await self._create_cache(func, *args, **kwargs)
+            elif self.refresh:
+                time_later.cancelled()
 
-            cached_data = type(self)._cache.get(key)
             logger.debug(f'The current cache: {type(self)._ttl=}')
             return cached_data
 
