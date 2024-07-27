bf0a5d5320454f1a40f8ad7ac1a03bc21f0daf67
IX <borntokill.ix@gmail.com>
2024-07-27 22:16:09 +0700

    tests(unittest): add new tests for autorefresh parametr

--- tests/tests_aiocache.py
+++ tests/tests_aiocache.py
@@ -7,6 +7,20 @@ from LogSettings import get_logger
 logger = get_logger('consolemode')
 
 
+class Later:
+    _loop = asyncio.get_event_loop()
+
+    def del_cache(self, key):
+        logger.debug(f'Remove cache: {key=}')
+
+    async def later(self):
+        loop = asyncio.get_event_loop()
+        logger.info(f'{loop=}')
+        logger.info(f'{self._loop=}')
+        loop.call_later(2, self.del_cache, 'long_api')
+        await asyncio.sleep(3)
+
+
 @asyncache(ttl=10)
 async def long_api():
     logger.debug('Load from async API')
@@ -14,7 +28,7 @@ async def long_api():
     return [{'namespace': 'A1'}, {'namespace': 'A2'}, {'namespace': 'A3'}]
 
 
-@asyncache(ttl=10)
+@asyncache(ttl=5)
 async def long_api_2():
     logger.debug('Load from async API')
     await asyncio.sleep(3)
@@ -23,23 +37,56 @@ async def long_api_2():
 
 class AiocacheTestCase(unittest.IsolatedAsyncioTestCase):
 
+    async def test_autorefresh(self):
+        result = await long_api()
+        print(f'Result: {result}')
+        result = await long_api()
+        print(f'Result: {result}')
+
+        print('time sleep...')
+        await asyncio.sleep(15)
+        print('time wakeup!')
+
+        result = await long_api()
+        print(f'Result: {result}')
+        result = await long_api()
+        print(f'Result: {result}')
+
+    async def test_autorefresh_two_corutines(self):
+        result = await long_api()
+        print(f'Result: {result}')
+        result = await long_api_2()
+        print(f'Result: {result}')
+        result = await long_api_2()
+        print(f'Result: {result}')
+
+        print('time sleep...')
+        await asyncio.sleep(15)
+        print('time wakeup!')
+
+        result = await long_api_2()
+        print(f'Result: {result}')
+        result = await long_api()
+        print(f'Result: {result}')
+
     async def test_aiocache(self):
         result = await long_api()
-        logger.info(f'Result: {result}')
+        print(f'Result: {result}')
         result = await long_api()
-        logger.info(f'Result: {result}')
+        print(f'Result: {result}')
 
         await asyncio.sleep(0.5)
 
         result = await long_api()
-        logger.info(f'Result: {result}')
+        print(f'Result: {result}')
         result = await long_api()
-        logger.info(f'Result: {result}')
+        print(f'Result: {result}')
 
     async def test_expire_aiocache(self):
         result = await long_api()
         print(f'Result: {result}')
-        result = await long_api_2()
+        await asyncio.sleep(10.0)
+        result = await long_api()
         print(f'Result: {result}')
 
         await asyncio.sleep(10.0)
